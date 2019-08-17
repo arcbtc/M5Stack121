33eca69c607d8499d9360bbf9155c67756f736c3
Arc <33088785+arcbtc@users.noreply.github.com>
2019-08-17 12:50:53 +0100

    Create M5Stack121BLITZ.ino

---
+++ M5Stack121BLITZ/M5Stack121BLITZ.ino
@@ -0,0 +1,233 @@
+#include "BLITZSplash.c" //calls opennode logo
+
+#include <M5Stack.h> 
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h> 
+
+//Wifi details
+char wifiSSID[] = "YOUR-WIFI";
+char wifiPASS[] = "YOUR-WIFI-PASSWORD";
+
+//BLITZ DETAILS
+const char*  server = "room77.raspiblitz.com"; //LAN ip could be used if on the same network
+const int httpsPort = 443; // standard ssl port
+String blitzport = ":8080"; //standard LND rest api port
+
+String readmacaroon = "YOUR-LND-READ-MACAROON";
+String invoicemacaroon = "YOUR-LND-INVOICE-MACAROON";
+const char* test_root_ca =   //SSL must be in this format, SSL for the node can be exported from yournode.com:8077 in firefox
+     "-----BEGIN CERTIFICATE-----\n" \
+    "MIICBTCCAaqgAwIBAgIQBSMZ9g3niBo1jyzK1DvECDAKBggqhkjOPQQDAjAyMR8w\n" \
+    "HQYDVQQKExZsbmQgYXV0b2dlbmVyYXRlZCBjZXJ0MQ8wDQYDVQQDEwZSb29tNzcw\n" \
+    "HhcNMTPBgNVHRMBAf8EBTADAQH/MHsGA1UdEQR0MHKCBMR8wHQYDVQQKExZsbmQg\n" \
+    "YXV0b2dlbmVyYXRlZCBjZXJ0MQ8wDQYDVQQDEwZSb29tNzcwWTATBgcqhkjOPQIB\n" \
+    "BggqhkjOPAAAAAAAAAAAAGHBMCoskqHEP6AAAAAAAAA+OWZzLshQoTUeV6FVKbFC\n" \
+    "CC+fVGRQsXJx+GVUknnNEcJTt/fQ9CmM6stqGPjAo4GhMIGeMA4GA1UdDwEB/wQE\n" \
+    "AwICpDAPBgNVHRMBAf8EBTADAQH/MHsGA1UdEQR0MHKCBlJvb203N4IJbG9jYWxo\n" \
+    "b3N0ghVyb29tNzcucmFzcGlibGl0ei5jb22CBHVuaXiCCnVuaXhwYWNrZXSHBH8A\n" \
+    "AAGHEAAAAAAAAAAAAAAAAAAAAAGHBMCoskqHEP6AAAAAAAAA+OWZZHfUV0qHBAAA\n" \
+    "AAAwCgYIKoZIzj0EAwIDSQAwRgIhALKz3oScii3i+5ltMVQc9u2O38rgfnGCj5Lh\n" \
+    "u9iwcAiZAiEA0BjRcisPUlG+SE/s+x6/A2NuT0gtIZ3PKd/GuM5T0jM=\n" \
+    "-----END CERTIFICATE-----\n";
+
+
+String invoiceamount = "200";
+bool settle = false;
+String payreq = "";
+String hash = "";
+
+void setup() {
+  M5.begin();
+  M5.Lcd.drawBitmap(0, 0, 320, 240, (uint8_t *)BLITZSplash_map);
+  
+  WiFi.begin(wifiSSID, wifiPASS);   
+  while (WiFi.status() != WL_CONNECTED) {
+  }
+  
+  pinMode(21, OUTPUT);
+}
+
+void loop() {
+
+  reqinvoice(invoiceamount);
+
+  M5.Lcd.fillScreen(BLACK); 
+  M5.Lcd.qrcode(payreq,45,0,240,10);
+  delay(100);
+  
+  gethash(payreq);
+  
+  checkpayment(hash);
+  int counta = 0;
+  while (counta < 1000){
+    if (settle == false){
+      delay(300);
+      checkpayment(hash);
+      counta++;
+    }
+    else{     
+      M5.Lcd.drawBitmap(0, 0, 320, 240, (uint8_t *)BLITZSplash_map);
+      digitalWrite(21, HIGH);
+      delay(15000);
+      digitalWrite(21, LOW);
+      delay(1000);
+      counta = 1000;
+    }  
+  }
+
+}
+
+////////////////////////// GET/POST REQUESTS///////////////////////////////
+
+void reqinvoice(String value){
+
+  String memo = "Room77-";
+  
+   WiFiClientSecure client;
+
+  client.setCACert(test_root_ca);
+
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(server, 8077))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+    
+   String topost = "{\"value\": \""+ value +"\", \"memo\": \""+ memo + String(random(1,1000)) +"\", \"expiry\": \"1000\"}";
+  
+       client.print(String("POST ")+ "https://" + server + blitzport + "/v1/invoices HTTP/1.1\r\n" +
+                 "Host: "  + server + blitzport +"\r\n" +
+                 "User-Agent: ESP322\r\n" +
+                 "Grpc-Metadata-macaroon:" + invoicemacaroon + "\r\n" +
+                 "Content-Type: application/json\r\n" +
+                 "Connection: close\r\n" +
+                 "Content-Length: " + topost.length() + "\r\n" +
+                 "\r\n" + 
+                 topost + "\n");
+
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        Serial.println("headers received"); 
+        break;
+      }
+    }
+    
+    String content = client.readStringUntil('\n');
+    Serial.println(content);
+
+    client.stop();
+    
+    const size_t capacity = JSON_OBJECT_SIZE(3) + 320;
+    DynamicJsonDocument doc(capacity);
+
+    deserializeJson(doc, content);
+
+    const char* r_hash = doc["r_hash"];
+    hash = r_hash;
+    const char* payment_request = doc["payment_request"]; 
+    payreq = payment_request;
+ 
+}
+}
+
+
+void gethash(String xxx){
+  
+   WiFiClientSecure client;
+
+  client.setCACert(test_root_ca);
+
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(server, 8077))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+
+       client.println(String("GET ") + "https://" + server + blitzport + "/v1/payreq/"+ xxx +" HTTP/1.1\r\n" +
+                 "Host: "  + server + blitzport +"\r\n" +
+                 "Grpc-Metadata-macaroon:" + readmacaroon + "\r\n" +
+                 "Content-Type: application/json\r\n" +
+                 "Connection: close");
+                 
+       client.println();
+
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        Serial.println("headers received"); 
+        break;
+      }
+    }
+    
+
+    String content = client.readStringUntil('\n');
+    Serial.println(content);
+
+
+    client.stop();
+
+    const size_t capacity = JSON_OBJECT_SIZE(7) + 270;
+    DynamicJsonDocument doc(capacity);
+
+    deserializeJson(doc, content);
+
+    const char* payment_hash = doc["payment_hash"]; 
+    hash = payment_hash;
+}
+}
+
+void checkpayment(String xxx){
+  
+   WiFiClientSecure client;
+
+  client.setCACert(test_root_ca);
+
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(server, 8077))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+
+       client.println(String("GET ") + "https://" + server + blitzport + "/v1/invoice/"+ xxx +" HTTP/1.1\r\n" +
+                 "Host: "  + server + blitzport +"\r\n" +
+                 "Grpc-Metadata-macaroon:" + readmacaroon + "\r\n" +
+                 "Content-Type: application/json\r\n" +
+                 "Connection: close");
+                 
+       client.println();
+
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        Serial.println("headers received"); 
+        break;
+      }
+    }
+    
+
+    String content = client.readStringUntil('\n');
+    Serial.println(content);
+
+    client.stop();
+    
+    const size_t capacity = JSON_OBJECT_SIZE(9) + 460;
+    DynamicJsonDocument doc(capacity);
+
+    deserializeJson(doc, content);
+
+    settle = doc["settled"]; 
+    
+  
+}
+}
+
+
+void page_qrdisplay(String xxx)
+{  
+
+  M5.Lcd.fillScreen(BLACK); 
+  M5.Lcd.qrcode(payreq,45,0,240,10);
+  delay(100);
+
+}
